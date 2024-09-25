42bf116d3d3544c81d239711dacb6afb581a32da
Mohak Barokar <mohak.barokar@ril.com>
2024-09-25 12:00:45 +0530

    Increased page load timeout for profile page

--- constants/profile-page-constants.ts
+++ constants/profile-page-constants.ts
@@ -1,5 +1,5 @@
 export const PROFILE_PAGE_CONSTANTS = {
     PROFILE_PAGE_TITLE: 'Pure progressive performance | Polestar',
     PROFILE_PAGE_LOAD_TIMEOUT: 60000,
-    CAR_CLEANUP_TIMEOUT: 15000
+    CAR_CLEANUP_TIMEOUT: 30000
 };
